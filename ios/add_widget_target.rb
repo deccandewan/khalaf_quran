f50721d8f75b279f88b592bb0eca3f83a8d39bcd
falafel <falafelvendor@gmail.com>
2026-07-22 01:33:45 +0530

    aislop

--- ios/add_widget_target.rb
+++ ios/add_widget_target.rb
@@ -20,33 +20,33 @@ begin
     puts "Creating target #{target_name}..."
 
     # Create the target
-    # Signature: new_target(product_type, name, platform = nil)
     target = project.new_target(:app_extension, target_name, :ios)
 
-    # Set the product type (this is often set by the new_target call but good to be explicit)
-    target.product_type = 'com.apple.product-type.app-extension'
-
     # Set build settings for both configurations
-    ['Debug', 'Release'].each do |config|
-      target.build_settings(config)['PRODUCT_BUNDLE_IDENTIFIER'] = 'com.abuhashim.khalaf_quran.QuranWidget'
+    ['Debug', 'Release'].each do |config_name|
+      config = target.build_configuration_list[config_name]
+      config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = 'com.abuhashim.khalaf_quran.QuranWidget'
+      config.build_settings['INFOPLIST_FILE'] = 'Runner/Widgets/Info.plist'
+      config.build_settings['CODE_SIGN_ENTITLEMENTS'] = 'Runner/Widgets/QuranWidget.entitlements'
+      config.build_settings['SWIFT_VERSION'] = '5.0'
+      config.build_settings['TARGETED_DEVICE_FAMILY'] = '1,2'
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
     end
 
-    # Add source files
+    # Find or create a group for the widget source files
+    widget_group = project.main_group.find_subpath('Runner/Widgets', true)
+    widget_group.set_source_tree('SOURCE_ROOT')
+
+    # Add source files (skip Info.plist/entitlements, those aren't build files)
     widget_files = Dir.glob('Runner/Widgets/*.swift')
     puts "Found widget files: #{widget_files.join(', ')}"
 
     widget_files.each do |file|
-      ref = project.objects.find { |o| o.respond_to?(:path) && o.path == file } || project.add_file(file)
-      target.add_build_file(ref)
+      file_name = File.basename(file)
+      file_ref = widget_group.files.find { |f| f.path == file_name }
+      file_ref ||= widget_group.new_file(file_name)
+      target.add_file_references([file_ref])
     end
-
-    # Set Info.plist
-    target.info_plist_file = 'Runner/Widgets/Info.plist'
-    puts "Set Info.plist to: #{target.info_plist_file}"
-
-    # Set Entitlements
-    target.entitlements_file = 'Runner/Widgets/QuranWidget.entitlements'
-    puts "Set Entitlements to: #{target.entitlements_file}"
 
     # Add target dependency to the main Runner target
     runner_target = project.targets.find { |t| t.name == 'Runner' }
